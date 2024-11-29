b76ef258c12d4e524e80888e5ffa26377ed4a036
kumashun8 <tiwo3wa4@gmail.com>
2024-11-29 08:41:01 +0000

    remove unused index

--- sql/alter.sql
+++ sql/alter.sql
@@ -1,2 +1,6 @@
 DROP INDEX `tenant_compet_player_id_idx` ON `visit_history`;
 ALTER TABLE `visit_history` ADD INDEX `tenant_compet_player_id_idx` (`tenant_id`, `competition_id`, `player_id`);
+
+-- tenant_id 単体でselectされていないので消す
+ALTER TABLE `visit_history` ADD INDEX `tenant_id_idx` (`tenant_id`);
+DROP INDEX `tenant_id_idx` ON `visit_history`;
